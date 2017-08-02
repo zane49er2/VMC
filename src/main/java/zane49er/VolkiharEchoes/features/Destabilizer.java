eedff95215f8e4bb1216c6b0902ed87d805e66bf
zane49er2 <zane49er2@gmail.com>
2017-08-02 14:32:53 -0400

    Sphere Test
    destabilizers should make spheres instead of flames

--- src/main/java/zane49er/VolkiharEchoes/features/Destabilizer.java
+++ src/main/java/zane49er/VolkiharEchoes/features/Destabilizer.java
@@ -18,7 +18,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Destabilizer extends Block {
 
 	boolean powered;
-	
+	int size = 1;
+	int particleRows = 10;
+	int particleLayers = 5;
+
 	public Destabilizer(String registryName) {
 		super(Material.PISTON);
 		setRegistryName(registryName);
@@ -28,20 +31,39 @@ public class Destabilizer extends Block {
 	}
 
 	@Override
-	//@SideOnly(Side.CLIENT)
+	// @SideOnly(Side.CLIENT)
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		//if (!worldIn.isBlockPowered(pos)) {
-			powered = worldIn.isBlockPowered(pos);
-		//}
+		// if (!worldIn.isBlockPowered(pos)) {
+		powered = worldIn.isBlockPowered(pos);
+		// }
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-		if (!powered) {
-			worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) pos.getX() + 0.5 , (double) pos.getY() + 2, (double) pos.getZ() + 0.5 , 0, 0, 0, new int[0]);
-			super.randomDisplayTick(stateIn, worldIn, pos, rand);
+		// if (powered) {
+		for (double theta = 0; theta < Math.PI*2; theta+= Math.PI / (particleRows/2)) {
+			for (double phi = 0; phi < Math.PI; phi+= Math.PI / particleLayers) {
+				double x = size * Math.cos(theta) * Math.sin(phi);
+				double y = size * Math.cos(phi);
+				double z = size * Math.sin(theta) * Math.sin(phi);
+
+				/*
+				 * int x = 1; int y = 1; int z = 1;
+				 */
+				if (0 < Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2) && 5 > Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) {
+					worldIn.spawnParticle(EnumParticleTypes.CRIT_MAGIC, (double) pos.getX() + 0.5 + x, (double) pos.getY() + 2 + y + size, (double) pos.getZ() + 0.5 + z, 0, 0, 0, new int[0]);
+
+				}
+			}
 		}
+		super.randomDisplayTick(stateIn, worldIn, pos, rand);
+		/*
+		 * } else { worldIn.spawnParticle(EnumParticleTypes.FLAME, (double)
+		 * pos.getX() + 0.5, (double) pos.getY() + 2, (double) pos.getZ() + 0.5,
+		 * 0, 0, 0, new int[0]); super.randomDisplayTick(stateIn, worldIn, pos,
+		 * rand); }
+		 */
 	}
 
 	@Override
